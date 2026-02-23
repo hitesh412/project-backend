b8290b1feee7f9b4580bafd18426ca9a2c308eea
hitesh412 <hiteshsarkale38@gmail.com>
2026-02-23 12:21:27 +0530

    update server file

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Backend is running.')
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
