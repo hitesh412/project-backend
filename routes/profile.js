980d5a0b77bd302ab6320c9542a73c086ba8953a
hitesh412 <hiteshsarkale38@gmail.com>
2026-03-03 13:57:17 +0530

    modify pages

--- routes/profile.js
+++ routes/profile.js
@@ -10,14 +10,14 @@ router.get("/", auth, async (req, res) => {
     const user = await User.findById(req.user.id).select("-password");
 
     // Get total orders of that user
-    const totalOrders = await Order.countDocuments({
+    const orders = await Order.countDocuments({
       user: req.user.id,
     });
 
     res.json({
       name: user.name,
       email: user.email,
-      totalOrders,
+      orders,
     });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
